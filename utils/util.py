0cfea32284c700a1e1a8210ce881f50709ef60ae
kensk8er <kensk8er1017@gmail.com>
2014-06-26 13:02:20 +0100

    created util function which write down similarity data into csv file.

--- utils/util.py
+++ utils/util.py
@@ -4,6 +4,7 @@ util module
 General utility functions are defined here.
 """
 import csv
+import sys
 
 __author__ = 'kensk8er'
 
@@ -25,22 +26,42 @@ def unpickle(file):
 
 
 def write_similarity_csv(pickle_indices, pickle_data, csv_name):
+    print 'loading pickled data...'
     indices = unpickle(pickle_indices)
     data = unpickle(pickle_data)
     writer = csv.writer(file(csv_name, 'w'))
+    count = 1
+    index_num = len(indices)
 
+    print 'writing csv file...'
+    print '\r', count, '/', index_num,
     # 1st row
     row = ['']
     for index in indices:
         row.append(index)
     writer.writerow(row)
+    count += 1
 
     # 2nd row and onwards
-    index_num = len(indices)
     for row_num in range(index_num):
+        print '\r', count, '/', index_num,
         row = [indices[row_num]]
 
         for col_num in range(index_num):
             row.append(data[row_num, col_num])
 
         writer.writerow(row)
+        count += 1
+    print '\r', count, '/', index_num,
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    assert len(args) > 1, 'at least one argument needed!'
+
+    if args[1] == 'write_similarity_csv':
+        assert len(args) == 5, '4 arguments needed for function write_similarity_csv!'
+        write_similarity_csv(pickle_indices=args[2], pickle_data=args[3], csv_name=args[4])
+
+    else:
+        print 'no function executed'
