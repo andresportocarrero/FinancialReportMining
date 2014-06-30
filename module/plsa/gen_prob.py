061af82e0b63862e984ae9f1247773b311d31172
kensk8er <kensk8er1017@gmail.com>
2014-07-01 00:56:29 +0100

    add functions to gen_prob.py

--- module/plsa/gen_prob.py
+++ module/plsa/gen_prob.py
@@ -62,8 +62,51 @@ def gen_p_w_z_w(p_w_z, p_w):
     return p_w_z / p_w.reshape((-1, 1))
 
 
+def gen_p_d(p_d_z, p_z):
+    """
+    Generate P(d) out of P(d|z) and P(z).
+
+    P(d) = Sigma_z[P(d|z) * P(z)]
+
+    :param p_d_z: P(d|z)
+    :param p_z: P(z)
+    :return: P(d)
+    """
+    assert len(p_z.shape) == 1, 'p_z needs to be 1-dimensional!'
+    assert len(p_d_z.shape) == 2, 'p_d_z needs to be 2-dimensional!'
+
+    return p_d_z.dot(p_z)
+
+
+def gen_p_dz(p_d_z, p_z):
+    """
+    Generate P(z,d) out of P(d|z) and P(z).
+
+    P(z,d) = P(d|z) * P(z)
+
+    :param p_d_z: P(d|z)
+    :param p_z: P(z)
+    :return: P(z,d)
+    """
+    return np.multiply(p_z, p_d_z)
+
+
+def gen_p_z_d(p_dz, p_d):
+    """
+    Generate P(z|d) out of P(d,z), and P(d).
+
+    Based on Bayes' rule: P(z|d) = P(d,z) / P(d)
+
+    :param p_dz: P(z,d)
+    :param p_d: P(d)
+    :return: P(z|d)
+    """
+    return (p_dz / p_d.reshape((-1, 1))).T
+
+
 if __name__ == '__main__':
     p_w_z = unpickle('result/plsa/p_w_z.pkl')
+    p_d_z = unpickle('result/plsa/p_d_z.pkl')
     p_z = unpickle('result/plsa/p_z.pkl')
 
     print 'computing P(w)...'
@@ -75,8 +118,17 @@ if __name__ == '__main__':
     print 'computing P(z|w)...'
     p_z_w = gen_p_z_w(p_wz, p_w)
 
-    print 'computing P(w|z) / P(w) = P(z,w) / {P(z) * P(w)}'
+    print 'computing P(w|z) / P(w) = P(z,w) / {P(z) * P(w)}...'
     p_w_z_w = gen_p_w_z_w(p_w_z, p_w)
+
+    print 'computing P(d)...'
+    p_d = gen_p_d(p_d_z, p_z)
+
+    print 'computing P(z,d)...'
+    p_dz = gen_p_dz(p_d_z, p_z)
+
+    print 'computing P(z|d)...'
+    p_z_d = gen_p_z_d(p_dz, p_d)
 
     print 'saving results into .pkl file...'
     enpickle(p_w, 'result/plsa/p_w.pkl')
