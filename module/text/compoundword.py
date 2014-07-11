cf9816d60779cc3a388c49fd9d7be0ac3ef73069
kensk8er <kensk8er1017@gmail.com>
2014-07-11 01:43:22 +0100

    improve pre-processing process, plus store the pre-processed data in gensim format.

---
+++ module/text/compoundword.py
@@ -0,0 +1,7 @@
+"""
+Self defined domain-specific compound-word list.
+"""
+__author__ = 'kensk8er'
+
+
+compound_words = ['morgan stanley', 'goldman sachs', 'jpmorgan chase', 'merrill lynch', 'kevin zolkiewicz']
